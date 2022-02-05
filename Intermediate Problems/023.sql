934c6187e6704734d35c2aeccdb2106b034a3a80
Hama Jokel <jokelhama@gmail.com>
2022-02-05 14:08:22 +0100

    Second revision

--- Intermediate Problems/023.sql
+++ Intermediate Problems/023.sql
@@ -1,4 +1,4 @@
-SELECT ProductID, ProductName, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued 
+SELECT ProductID, ProductName, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued
 FROM Products
 WHERE (UnitsInStock + UnitsOnOrder) <= ReorderLevel AND Discontinued = 0
-ORDER BY ProductID
+ORDER BY ProductID 
