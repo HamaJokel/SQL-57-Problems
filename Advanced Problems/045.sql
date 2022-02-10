1b4795f6b1bb52621e58d36b48d336049d30c832
Hama Jokel <jokelhama@gmail.com>
2022-02-10 19:02:21 +0100

    second revision

--- Advanced Problems/045.sql
+++ Advanced Problems/045.sql
@@ -1,21 +1,36 @@
-WITH AllOrders AS( --All Orders
-	SELECT E.EmployeeID, COUNT(*) AS AllOrders
-	FROM Employees E
-	LEFT JOIN Orders O 
-	ON E.EmployeeID = O.EmployeeID
-	GROUP BY E.EmployeeID
-),  LateOrders AS( --Late Orders
-		SELECT E.EmployeeID, COUNT(*) AS LateOrders
-		FROM Employees E
-		LEFT JOIN Orders O 
-		ON E.EmployeeID = O.EmployeeID
-		WHERE O.RequiredDate <= O.ShippedDate
-		GROUP BY E.EmployeeID, E.LastName
-	) 
-SELECT E.EmployeeID, E.LastName, AllOrders.AllOrders, ISNULL(LateOrders.LateOrders, 0) AS LateOrders
-FROM Employees E
-LEFT JOIN LateOrders
-ON E.EmployeeID = LateOrders.EmployeeID
-JOIN AllOrders 
-ON E.EmployeeID = AllOrders.EmployeeID
-ORDER BY EmployeeID
+WITH AllOrders AS(
+  SELECT 
+    O.EmployeeID, 
+    E.LastName, 
+    COUNT(*) AS TotalOrders 
+  From 
+    Orders O 
+    JOIN Employees E ON O.EmployeeID = E.EmployeeID 
+  GROUP BY 
+    O.EmployeeID, 
+    E.LastName
+), 
+LateOrders AS(
+  SELECT 
+    O.EmployeeID, 
+    E.LastName, 
+    COUNT(*) AS LateOrders 
+  FROM 
+    Orders O 
+    JOIN EMPLOYEES E ON O.EmployeeID = E.EmployeeID 
+  WHERE 
+    ShippedDate >= RequiredDate 
+  GROUP BY 
+    O.EmployeeID, 
+    E.LastName
+) 
+SELECT 
+  AllOrders.EmployeeID, 
+  AllOrders.LastName, 
+  AllOrders.TotalOrders, 
+  COALESCE(LateOrders.LateOrders, 0) AS LateOrders 
+FROM 
+  AllOrders 
+  LEFT JOIN LateOrders ON AllOrders.EmployeeID = LateOrders.EmployeeID 
+ORDER BY 
+  AllOrders.EmployeeID
