f35485cf09821a3cd16c45e54d941dd902f4db85
Hama Jokel <jokelhama@gmail.com>
2022-02-10 18:39:21 +0100

    second revision

--- Advanced Problems/042.sql
+++ Advanced Problems/042.sql
@@ -1,10 +1,14 @@
-SELECT E.employeeid, 
-       E.lastname, 
-       Count(*) AS TotalLateOrders 
-FROM   orders O 
-       JOIN employees E 
-         ON O.employeeid = E.employeeid 
-WHERE  O.requireddate <= O.shippeddate 
-GROUP  BY E.employeeid, 
-          E.lastname 
-ORDER  BY totallateorders DESC; 
+SELECT 
+  O.EmployeeID, 
+  E.LastName, 
+  COUNT(*) AS TotalLateOrders 
+FROM 
+  Orders O 
+  JOIN Employees E ON O.EmployeeID = E.EmployeeID 
+WHERE 
+  ShippedDate >= RequiredDate 
+GROUP BY 
+  O.EmployeeID, 
+  E.LastName 
+ORDER BY 
+  TotalLateOrders DESC
