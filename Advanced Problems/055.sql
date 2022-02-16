71e275a4b93427a2fe74157ed541b000e092596e
Hama Jokel <jokelhama@gmail.com>
2022-02-16 21:50:06 +0100

    second revision

--- Advanced Problems/055.sql
+++ Advanced Problems/055.sql
@@ -1,15 +1,25 @@
-WITH CTE AS
-		(
-			SELECT 
-			ROW_NUMBER() OVER(
-			PARTITION BY ShipCountry
-			ORDER BY ShipCountry) as row_num,
-			ShipCountry, 
-			CustomerID, 
-			OrderID, 
-			OrderDate = CONVERT(DATE, orderdate) 
-			FROM Orders
-		)
-SELECT ShipCountry, CustomerID, OrderID, OrderDate 
-FROM CTE
-WHERE row_num = '1'
+WITH CTE AS(
+  Select 
+    ShipCountry, 
+    CustomerID, 
+    OrderID, 
+    OrderDate = convert(date, OrderDate), 
+    ROW_NUMBER() OVER(
+      PARTITION BY ShipCountry 
+      ORDER BY 
+        ShipCountry
+    ) row_num 
+  FROM 
+    orders
+) 
+SELECT 
+  ShipCountry, 
+  CustomerID, 
+  OrderID, 
+  OrderDate = CONVERT(DATE, Orderdate) 
+FROM 
+  CTE 
+WHERE 
+  row_num = 1 
+ORDER BY 
+  ShipCountry
