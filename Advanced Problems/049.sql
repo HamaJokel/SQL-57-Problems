7694265083eeea53491675bbb126a4a7d0ba9931
Hama Jokel <jokelhama@gmail.com>
2022-02-16 21:00:59 +0100

    second revision

--- Advanced Problems/049.sql
+++ Advanced Problems/049.sql
@@ -1,22 +1,27 @@
-WITH cte AS(
-	SELECT O.CustomerID, C.CompanyName, SUM(OD.UnitPrice * OD.Quantity) AS TotalOrderAmount
-	FROM OrderDetails OD
-	JOIN Orders O
-	ON OD.OrderID = O.OrderID
-	AND CONVERT(DATE, O.OrderDate) BETWEEN '2016' AND '2017'
-	JOIN Customers C
-	ON O.CustomerID = C.CustomerID
-	GROUP BY O.CustomerID, C.CompanyName
-	
-)
-SELECT *, 
-CASE
-	WHEN TotalOrderAmount BETWEEN 0 AND 1000 THEN 'Low'
-	WHEN TotalOrderAmount BETWEEN 1001 AND 5000 THEN 'Medium'
-	WHEN TotalOrderAmount BETWEEN 5001 AND 10000 THEN 'High'
-	ELSE 'Very High'
-END AS CustomerGroup
-FROM cte
-
-ORDER BY cte.CustomerID
-
+WITH CTE AS(
+  SELECT 
+    O.CustomerID, 
+    C.CompanyName, 
+    SUM(Quantity * UnitPrice) AS TotalOrderAmount 
+  FROM 
+    Orders O 
+    JOIN OrderDetails OD ON O.OrderID = OD.OrderID 
+    JOIN Customers C ON O.CustomerID = C.CustomerID 
+  WHERE 
+    YEAR(O.OrderDate) = 2016 
+  GROUP BY 
+    O.CustomerID, 
+    C.CompanyName
+) 
+SELECT 
+  CTE.CustomerID, 
+  CTE.CompanyName, 
+  COALESCE(CTE.TotalOrderAmount, 0) AS TotalorderAmount, 
+  CASE WHEN TotalOrderAmount BETWEEN 0 
+  AND 1000 THEN 'Low' WHEN TotalOrderAmount BETWEEN 1000 
+  AND 5000 THEN 'Medium' WHEN TotalOrderAmount BETWEEN 5000 
+  AND 10000 THEN 'High' ELSE 'Very High' END AS 'CustomerGroup' 
+FROM 
+  CTE 
+ORDER BY 
+  CustomerID
